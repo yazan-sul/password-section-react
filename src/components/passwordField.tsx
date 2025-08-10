32712bd248a21cb18565d95fe019bf7761d26697
yazan-sul <yazan-sul@gmail.com>
2025-08-10 15:05:18 +0300

    add visibility toggle

--- src/components/passwordField.tsx
+++ src/components/passwordField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 type Props = {
@@ -6,18 +6,26 @@ type Props = {
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
+
 export function PasswordField({ label, value, onChange }: Props) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   return (
     <div className="password-field">
       <label>{label}</label>
-      <input 
-        type="password"
-        placeholder={`Enter your ${label}`} 
+      <input
+        type={showPassword ? "text" : "password"}
+        placeholder={`Enter your ${label}`}
         value={value}
         onChange={onChange}
-        
       />
+      <button type="button" id="password-toggle" className={`${showPassword ? "hide" : "show"}`} onClick={togglePasswordVisibility}>
+        👁️
+      </button>
     </div>
   );
 }
